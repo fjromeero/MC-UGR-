076c55be84ff54d1f701d7c275f682146775d244
franjaviro2001@gmail.com <fjromeero@correo.ugr.es>
2021-12-18 19:12:50 +0100

    2

--- elarchivoc.cc
+++ elarchivoc.cc
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <curl/curl.h>
+#include <string>
+
+static size_t Convertir_a_texto(void *cont,size_t size, size_t nmemb, void *userp){
+    ((std::string*)userp)->append((char*)cont, size *nmemb);
+    return size * nmemb;
+}
+
+using namespace std;
 
 int main(){
     CURL *curl;
     CURLcode res;
-    curl_global_init(CURL_GLOBAL_ALL);
+    std::string leer;
+    fstream archivo;
 
     curl = curl_easy_init();
     if(curl){
-        curl_easy_setopt(curl,CURLOPT_URL, "https://www.google.com");
-
+        curl_easy_setopt(curl, CURLOPT_URL,"https://git-scm.com/download/linux");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,Convertir_a_texto);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA,&leer);
         res = curl_easy_perform(curl);
-
-        if(res!= CURLE_OK)
-            fprintf(stderr,"curl_easy_perform() returned %s\n",curl_easy_strerror(res));
-
         curl_easy_cleanup(curl);
     }
 
-    curl_global_cleanup();
-    return 0;
+    archivo.open("/home/fjromeero/Documentos/GitHub/MC-UGR-/http.txt");
+    if(!archivo.is_open()){
+        cout<<"NO SE ABRE EL ARCHIVO";
+    }
+    archivo << leer;
 }
